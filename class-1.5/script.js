fb75c781cc5a87ea2db6dcbce359d582e0a3b255
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-09 01:02:55 +0600

    class-1.5
     class 1.5 added

---
+++ class-1.5/script.js
@@ -0,0 +1,34 @@
+console.log("Hello world");
+
+var a = "Let's have fun";
+
+console.log(a);
+
+var b = 'Let\' s do something';
+console.log(b);
+
+var c = 'Let\n system';
+console.log(c);
+
+var d = `lkdjsflksdjflk
+lkjsdlkfjsdlkfj
+ldkjflksdjf
+iurhbkjhkj`;
+console.log(d);
+
+var e = 1564;
+var f = `oiuyweroiueworu
+oiuyerioue ${e} wio
+werufoiwequr`;
+
+console.log(f);
+
+var g = "ieurhnxc.mkx " + 1525;
+
+console.log(g);
+
+var h = "Please" + " pass" + " the" + " glass";
+console.log(h);
+
+var i = "এখন গভীর রাত।";
+console.log(i);
