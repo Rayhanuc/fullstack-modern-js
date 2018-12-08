b83c699f6ae2f125a365265eb7161fb852276852
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-08 21:02:43 +0600

    class 1.3 and class 1.4
    Class 1.3 index.html file added and class 1.4 index.html and number1.js to number4.js file added

---
+++ class-1.4/script.js
@@ -0,0 +1,24 @@
+var a = 1971;
+
+console.log(a+200);
+console.log(a-500);
+console.log(a*25);
+console.log(a/15);
+
+var kuddus = 2018;
+console.log(kuddus);
+
+// can't use [ 5jungle = 500; ]
+// can't use [ 5ju-ngle = 500; ]
+
+var _keka = 2564;
+
+console.log(_keka);
+
+const bangladesh = 1526;
+
+console.log(bangladesh);
+
+var a = 5;
+
+console.log(a);
