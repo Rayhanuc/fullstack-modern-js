e98285233f25b055c6e3baf4424b3e26ece03c88
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-10 00:40:12 +0600

    class-1.6
    class-1.6 added

---
+++ class-1.6/script.js
@@ -0,0 +1,60 @@
+// carAt function
+var a = "Hello world";
+var b = a.charAt(2);
+console.log(b);
+
+// carAt function
+var c = "Something";
+var d = c.charAt(2);
+console.log(d);
+
+// Letter position
+var e = "Something";
+var f = e.charCodeAt(2);
+console.log(f);
+
+// Letter position
+var g = "Mango";
+var h = g.indexOf('g');
+console.log(h);
+
+// Matching
+var i = "Mango";
+var j = i.match('go');
+console.log(j);
+
+// Letter missing
+var k = "Mango";
+var l = k.match('bhjhjk');
+console.log(l);
+
+// Letter replace
+var m = "Come";
+var n = m.replace('Come', "Go");
+console.log(n);
+
+// Word/letter length (Hel lo = 6 letter)
+var o = "Come";
+var p = o.length;
+console.log(p);
+
+// Uppercase
+var q = "Hi! There";
+var r = q.toUpperCase();
+console.log(r);
+
+// Lowercase
+var s = "SOMETHING IS BETTER THEN NOTHING";
+var t = s.toLowerCase();
+console.log(t);
+
+// Lowercase
+var u = ("hello" ==="hello");
+// var t = s.toLowerCase();
+console.log(u);
+
+// Lowercase
+var v = " DaasdDs ";
+var w = "daldfjlsk"
+var x = v.trim( ).toLowerCase();
+console.log(x);
