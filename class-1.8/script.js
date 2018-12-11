8112b1ebe37e386bda587b4aeb32986e2d282620
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-12 00:39:41 +0600

    class-1.7 and class-1.8
    class-1.7 script.js link updated and class-1.8 folder added.

---
+++ class-1.8/script.js
@@ -0,0 +1,17 @@
+var a = 15+5;
+console.log(a);
+
+var b = 22-8;
+console.log(b);
+
+var c = 15*3;
+console.log(c);
+
+var d = 15/9;
+console.log(d);
+
+var e = 15%4;
+console.log(e);
+
+var f = "Hello" + " world";
+console.log(f);
