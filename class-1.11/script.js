3ef1bb7fd19f487723ac3cc465ba620eea32ca16
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-19 21:22:17 +0600

    class-1.11
    class-1.11 folder added.

---
+++ class-1.11/script.js
@@ -0,0 +1,138 @@
+
+
+
+if (true) {
+	console.log("আমি ভাল আছি।");
+}else {
+	console.log("আমি ভাল নেই।");
+} 
+
+
+	
+var age = 22;
+
+console.log(age>=18);
+
+if (age >= 18) {
+	console.log("Biar boyosh hoisa :)");
+}else {
+	console.log("Biar boyosh hoini! :(");
+}
+
+console.log("age" == "age");
+
+var age = 22;
+
+if (age <= 18) {
+	console.log("Biar boyosh hoisa :)");
+}else {
+	console.log("Biar boyosh hoini! :(");
+}
+
+
+
+if (false) {
+	console.log("Hello world");
+}else {
+	console.log("Not hello world");
+}
+
+
+var b = 19;
+
+if (b >= 21) {
+	console.log("ছেলে হলে বিয়ের বয়স হয়েছে।");
+}
+else if(b >= 18) {
+	console.log("মেয়ে হলে বিয়ের বয়স হয়েছে।");
+}
+
+else {
+	console.log("বিয়ের বয়স হয়নি।");
+}
+
+
+var c = 25;
+
+if (c >= 21) {
+	console.log("ছেলে হলে বিয়ের বয়স হয়েছে।");
+}
+else if(c >= 18) {
+	console.log("মেয়ে হলে বিয়ের বয়স হয়েছে।");
+}
+
+else {
+	console.log("বিয়ের বয়স হয়নি।");
+}
+
+
+var d = 17;
+
+if (d >= 21) {
+	console.log("ছেলে হলে বিয়ের বয়স হয়েছে।");
+}
+else if(d >= 18) {
+	console.log("মেয়ে হলে বিয়ের বয়স হয়েছে।");
+}
+
+else {
+	console.log("বিয়ের বয়স হয়নি।");
+}
+
+
+
+var a = 15;
+
+if (a >= 21) {
+	console.log("ছেলে হলে বিয়ের বয়স হয়েছে।");
+}
+else if(a >= 18) {
+	console.log("মেয়ে হলে বিয়ের বয়স হয়েছে।");
+}
+
+else if(a >= 15) {
+	console.log("বিয়ে করার জন্য তোমাকে আরও তিন বছর বড় হতে হবে।");
+}
+
+else {
+	console.log("বিয়ের বয়স হয়নি।");
+}
+
+
+var b = 10;
+
+if (b >= 21) {
+	console.log("ছেলে হলে বিয়ের বয়স হয়েছে।");
+}
+else if(b >= 18) {
+	console.log("মেয়ে হলে বিয়ের বয়স হয়েছে।");
+}
+
+else if(b >= 10) {
+	console.log("তোমার এখনও বিয়ের বয়স হয়নি।");
+}
+
+else {
+	console.log("বিয়ের বয়স হয়নি।");
+}
+
+
+var c = 5;
+
+if (c >= 21) {
+	console.log("ছেলে হলে বিয়ের বয়স হয়েছে।");
+}
+else if(c >= 18) {
+	console.log("মেয়ে হলে বিয়ের বয়স হয়েছে।");
+}
+
+else if(c >= 5) {
+	console.log("তুমি এখনও শিশু, তোমার বিয়ের বয়স হয়নি।");
+}
+
+else {
+	console.log("বিয়ের বয়স হয়নি।");
+}
+
+
+console.log("একেবারে শেষ লাইন।");
