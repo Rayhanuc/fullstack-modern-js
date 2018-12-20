ef3859f9b7d578825ceab604ecca07eb70677540
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-20 15:43:02 +0600

    class-1.12
    This class topic is if statement and ternarly operator.

---
+++ class-1.12/script.js
@@ -0,0 +1,25 @@
+
+var age = 20;
+
+if (age>18)
+	console.log("সাবালক");
+else
+	console.log("নাবালক");
+
+/*  ternary operator
+    Syntex
+    condition?/ternary? true : false;
+    টারনারী অপারেটর তিনটা ভেলু/এক্সপ্রেশান নিয়ে কাজ করে।
+*/
+var a = 16;
+a>18? console.log("সাবালক") : console.log("নাবালক");
+
+var b = 25;
+b>20? console.log("কাজের জন্য উপযোক্ত।") : console.log("কাজের জন্য উপযোক্ত হয় নি।");
+
+var c = 20;
+c>17? console.log("কাজের জন্য উপযোক্ত হয়েছে।") : console.log("কজের জন্য উপযোক্ত হতে তুমাকে আরও বড় হতে হবে।");
+
+var c = 19;
+c>22? console.log("কাজের জন্য উপযোক্ত হয়েছে।") : console.log("কজের জন্য উপযোক্ত হতে তুমাকে আরও বড় হতে হবে।");
+
