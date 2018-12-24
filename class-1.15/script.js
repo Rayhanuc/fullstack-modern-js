db9fbfb2d794babf55db633e3a6547248d3f3530
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-24 23:32:40 +0600

    class-1.15
    Loop Ingroduction.

---
+++ class-1.15/script.js
@@ -0,0 +1,36 @@
+/*
+১.১৫ - লুপ পরিচিতি
+*/
+
+// This loop for browser crash ! Don't try this loop.
+
+// while(1) {
+//     console.log("BOO");
+// }
+
+var a = 0;
+while(a<5) {
+    console.log(a);
+    a++;//a=a+1 short form
+    console.log("BOO");
+}
+console.log("hello");
+
+var b = 0;
+do{
+    console.log(b);
+    b++;//a=a+1 short form
+    console.log("BOO");
+}while(b<5)
+console.log("hello");
+
+var c = 2;
+while (c<15) {
+    console.log(c);
+    c++;
+    console.log("BOO");
+    console.log("Goom");
+    console.log("My Name is Rayhan");
+    console.log(" ");
+}
+console.log("World");
