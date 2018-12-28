19873118d13d2d99cf78e88a66de2c801db5897b
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-28 14:17:30 +0600

    class-1.17
    For loop explain

---
+++ class-1.17/script.js
@@ -0,0 +1,36 @@
+/*
+১.১৭ - ফর লুপ
+*/
+
+// for(initial;condition;change){}
+console.log("Condition 1");
+let a = 0;
+while(a<=10) {
+    console.log(a);
+    a++; // a=a+1 same
+}
+
+// for loop condition
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];    
+// }
+console.log("Condition 2");
+for (let b = 0; b <= 10; b++) {
+    console.log(b);    
+}
+
+// c+=1 || c = c+1
+console.log("Condition 3");
+for (let c = 1; c <=100; c+=1) {
+    console.log(c); 
+}
+
+console.log("Condition 4");
+for (let d = 2; d <=100; d+=2) {
+    console.log(d); 
+}
+
+console.log("Condition 5");
+for (let f = 100; f>0; f-=2) {
+    console.log(f); 
+}
