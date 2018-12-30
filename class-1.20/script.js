ca187779acbde4a0d4242c1586f9c6e30a503eaf
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2018-12-30 20:47:59 +0600

    class-1.20
    Condition project

---
+++ class-1.20/script.js
@@ -0,0 +1,33 @@
+/*
+১.২০ - কন্ডিশন প্রোজেক্ট
+*/
+
+
+var msg = document.querySelector('#message');
+var btn = document.querySelector('#interact');
+var result = document.querySelector('#answer');
+
+btn.addEventListener('click', function(){
+    let c = msg.value.toLowerCase();
+    let reply = "";
+    if (c.match('hello')) {
+        reply = "hello there!";
+    }
+    else if(c.match('sing for me')) {
+        reply = "Ami banglai gan gai, ami banglar gan gai. ami amar ami ka shiro din ai banglai koja pai";
+    }
+    else if(c.match('tomar nam ki')) {
+        reply = "Amar nam Kuddus Bot";
+    }
+    else if(c.match('amar nam')) {
+        reply = "Rayhan Uddin Chowdhury";
+    }
+    else {
+        reply = "Boji nai";
+    }
+    result.innerHTML = reply;
+
+    var kotha = new SpeechSynthesisUtterance(reply);
+    window.speechSynthesis.speak(kotha);
+});
+
