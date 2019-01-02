ef670cb85030e259efb23856a50c6af8ba336d60
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-01-03 00:19:51 +0600

    class-1.24
    Function expression

---
+++ class-1.24/script.js
@@ -0,0 +1,15 @@
+/*
+১.২৪ - ফাংশন এক্সপ্রেশন
+*/
+
+// Function expression
+var add = function(a,b) {
+            return a+b;
+          }
+
+console.log(add(10,7));
+
+// function declaration
+function addingfunction() {
+
+}
