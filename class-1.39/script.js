9d044b8047fdfb19c4f4d637fd634debf5147979
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-01-23 00:04:19 +0600

    class-1.39
    Mistake find-out and make it right.

---
+++ class-1.39/script.js
@@ -0,0 +1,13 @@
+/*
+১.৩৯ - ভদ্রলোকের মত ভুল শুধরানো (Error Handling)
+*/
+
+// throw new Error("Hello world");
+
+try{
+colole.log("ljdklsfj");
+}catch (e) {
+    console.log(e);
+}
+
+console.log("cool!");
