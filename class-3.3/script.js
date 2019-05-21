69db27cd7dbe13259c812e3a5fc7df07613f067c
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-05-21 16:07:05 +0600

    JavaScript
    ৩.৩ - ডম এলিমেন্ট এ্যড অথবা রিমুভ করা

---
+++ class-3.3/script.js
@@ -0,0 +1,33 @@
+/*
+৩.৩ - ডম এলিমেন্ট এ্যড অথবা রিমুভ করা
+*/
+
+
+
+/*
+
+var container = document.querySelector('.a');
+container;
+container.innerHTML;
+container.innerHTML= container.innerHTML+"<i>hello world</i>";
+container.querySelector('i');
+container.innerHTML="";
+
+
+
+
+
+var newEl = document.createElement('div')
+undefined
+newEl.style.color="#f00"
+"#f00"
+newEl.innerHTML="hellow world2"
+"hellow world2"
+document.querySelector('.a').appendChild(newEl)
+<div style=​"color:​ rgb(255, 0, 0)​;​">​hellow world2​</div>​
+newEl
+<div style=​"color:​ rgb(255, 0, 0)​;​">​hellow world2​</div>​
+document.querySelector('.a').removeChild(newEl)
+<div style=​"color:​ rgb(255, 0, 0)​;​">​hellow world2​</div>​
+
+*/
