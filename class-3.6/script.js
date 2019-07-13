5fd4120c65887a84314b6932a479643c77b18f69
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-13 18:22:00 +0600

    3.6 script.js file edited

--- class-3.6/script.js
+++ class-3.6/script.js
@@ -1,25 +1,4 @@
 /*
-৩.৫ - ডম প্রোজেক্ট ০১
+৩.৬ - ডাটা সিরিয়ালাইজ করা, ব্রাউজারে স্টোর করে রাখা
 */
 
-var inp = document.querySelector(".taskname");
-var list = document.querySelector(".tasklist");
-var tasklist = [];
-
-// ["get up"."brush teeth"]
-function render(elements) {
-	list.innerHTML = "";
-  elements.forEach(e => {
-    let newEl = document.createElement("li");
-    newEl.innerHTML = e;
-    list.appendChild(newEl);
-  });
-}
-
-inp.addEventListener("keyup", e => {
-  if (e.which === 13 && inp.value !== "") {
-    tasklist.push(inp.value);
-    inp.value = "";
-    render(tasklist);
-  }
-});
