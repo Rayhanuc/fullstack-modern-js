9f09ac3ffc83f959c7f75c2bf2ee126c00d1fcd2
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-13 19:53:20 +0600

    ৩.৮ - ব্রাউজারের কিছু মজার বিল্ট ইন এপিআই
    ৩.৮ - ব্রাউজারের কিছু মজার বিল্ট ইন এপিআই

--- class-3.8/script.js
+++ class-3.8/script.js
@@ -1,3 +1,23 @@
 /*
 ৩.৮ - ব্রাউজারের কিছু মজার বিল্ট ইন এপিআই
 */
+
+let vid = document.querySelector(".myvid");
+
+function success(stream) {
+	let vid_url = URL.createObjectURL(stream);
+	vid.src = vid_url;
+	vid.play();
+}
+
+function fail(err) {
+	console.log(err.message);
+}
+
+navigator.mediaDevices
+	.getUserMedia({
+		video: true,
+		audio: true
+	})
+	.then(success)
+	.catch(fail);
