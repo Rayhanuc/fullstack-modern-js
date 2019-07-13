e352dccc7c6e3904bbd6234da600f0e845915ead
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-14 01:22:38 +0600

    ৩.৯ - ক্যানভাস পরিচিতি
    ৩.৯ - ক্যানভাস পরিচিতি

---
+++ class-3.9/script.js
@@ -0,0 +1,15 @@
+/*
+৩.৯ - ক্যানভাস পরিচিতি
+*/
+
+var cvs = document.querySelector("canvas");
+
+var ctx = cvx = cvs.getContext("2d");
+
+
+ctx.fillStyle = "#0f0";
+ctx.fillRect(10,10,50,50);
+
+ctx.strokeStyle="#f00";
+ctx.strokeRect(5,5,40,40);
+
