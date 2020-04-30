ad0998645e15550105e61c7e2c73537a834af298
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-05-01 05:24:53 +0600

    3.14 - AJAX Project, Creating Weather Applications
    3.14 - AJAX Project, Creating Weather Applications

---
+++ class-3.14/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('.action');
+const result = document.querySelector('.result');
+
+// API RELATED CONSTANTS
+const BASE = "http://api.weatherstack.com/";
+const KEY = "32d21ebaeca4813649bac17aec078a70";
+const PLACE = "Chittagong";
+
+
+btn.addEventListener('click', f=>{
+	fetch(`${BASE}current?access_key=${KEY}&query=${PLACE}`)
+		.then(response => response.json())
+		.then(forecast=>{
+			// result.innerHTML = `The weather in ${forecast.location.name}`
+			console.log(forecast);
+			result.innerHTML = `<h1 style="color:green;">The weather in ${forecast.location.name} is ${forecast.current.temperature} Degree Celcius</h1>`
+		}
+		)
+});
